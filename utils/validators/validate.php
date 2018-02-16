f277547284eda6c1f344a9f1564320e5c1329aaf
Bp. Zuglói Gyülekezet <zuglogyuli.technika@gmail.com>
2018-02-16 22:40:48 +0100

    Hibajavítás: az ellenőrző kódok futtathatók legyenek más mappából is

--- utils/validators/validate.php
+++ utils/validators/validate.php
@@ -8,7 +8,7 @@ set_error_handler(
     }
 );
 
-foreach (glob ('src/*.php') as $validator) {
+foreach (glob (dirname(__FILE__).'/src/*.php') as $validator) {
     echo(str_repeat('-', 50)."\n");
     try {
         require($validator);
