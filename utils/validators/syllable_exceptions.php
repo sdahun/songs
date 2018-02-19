9d28db0c71df402be500e324fbca07c95ad46a03
Bp. Zuglói Gyülekezet <zuglogyuli.technika@gmail.com>
2018-02-19 13:34:28 +0100

    Új énekeskönyv: Szent az Úr

--- utils/validators/syllable_exceptions.php
+++ utils/validators/syllable_exceptions.php
@@ -22,6 +22,41 @@ $linecount_exceptions = [
     'szent_az_ur/005.xml;v3;1',
 
     'szent_az_ur/007.xml;v2;3',
-];
+
+    'szent_az_ur/008.xml;v2;1',
+    'szent_az_ur/008.xml;v2;2',
+    'szent_az_ur/008.xml;v2;3',
+    'szent_az_ur/008.xml;v2;4',
+    'szent_az_ur/008.xml;v2;5',
+
+    'szent_az_ur/009.xml;v2;5',
+
+    'szent_az_ur/010.xml;v2;1',
+
+    'szent_az_ur/011.xml;v2;1',
+    'szent_az_ur/011.xml;v2;2',
+    'szent_az_ur/011.xml;v2;3',
+    'szent_az_ur/011.xml;v2;5',
+
+    'szent_az_ur/012.xml;v2;1',
+
+    'szent_az_ur/022.xml;v2;1',
+    'szent_az_ur/022.xml;v2;3',
+
+    'szent_az_ur/026.xml;v2;2',
+    'szent_az_ur/026.xml;v3;4',
+
+    'szent_az_ur/027.xml;v2;3',
+    'szent_az_ur/027.xml;v2;4',
+
+    'szent_az_ur/028.xml;v2;1',
+    'szent_az_ur/028.xml;v3;1',
+    'szent_az_ur/028.xml;v3;2',
+
+    'szent_az_ur/031.xml;v2;4',
+
+    'szent_az_ur/038.xml;v2;1',
+    'szent_az_ur/038.xml;v2;3',
+  ];
   
   
