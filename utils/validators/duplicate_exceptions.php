e1ba1d8fb5b9023b6abd90657db4604def408f8d
Bp. Zuglói Gyülekezet <zuglogyuli.technika@gmail.com>
2018-04-13 10:14:06 +0200

    Duplikált ének ellenőrző az ének címe alapján

---
+++ utils/validators/duplicate_exceptions.php
@@ -0,0 +1,7 @@
+<?php
+
+//list of songs, which has the same title, but contents are totally different
+$duplicate_exceptions = [
+  'hitunk_enekei/005.xml' => 'hitunk_enekei/007.xml',
+  'hitunk_enekei/007.xml' => 'hitunk_enekei/005.xml',
+];
