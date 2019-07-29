49170b9bdbf7ae368e28f880c1b47a74c915eee6
Bp. Zuglói Gyülekezet <zuglogyuli.technika@gmail.com>
2019-07-29 19:07:59 +0200

    Hibajavítás a gyűjtemény rövidítésekhez

--- utils/compilers/Collections.php
+++ utils/compilers/Collections.php
@@ -11,14 +11,15 @@ class Collections {
             case 'Adj zengő éneket!':                return 'A';
             case 'Baptista gyülekezeti énekeskönyv': return 'B';
             case 'Dicsérem Neved 1':                 return 'T';
-            case 'Dicsérem Neved 2':                 return 'U';
-            case 'Dicsérem Neved 3':                 return 'V';
-            case 'Dicsérem Neved 4':                 return 'W';
-            case 'Dicsérem Neved 5':                 return 'X';
+            case 'Dicsérem Neved 2':                 return 'V';
+            case 'Dicsérem Neved 3':                 return 'W';
+            case 'Dicsérem Neved 4':                 return 'X';
+            case 'Dicsérem Neved 5':                 return 'Y';
             case 'Erőm és énekem az Úr':             return 'E';
             case 'Hitünk énekei':                    return 'H';
             case 'Hozsánna énekes':                  return 'O';
             case 'Szent az Úr':                      return 'S';
+            case 'Üdv- és adventi énekek':           return 'U';
             default: throw new Exception('Please add "' . $name . '" to the quick search name list!');
         }
     }
